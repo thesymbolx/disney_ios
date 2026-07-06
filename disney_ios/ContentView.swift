656b8e118e7b558cd823ab3ad4c1d9f91567c884
Dale Evans <dale.evans1984@gmail.com>
2026-07-06 10:28:16 +0100

    something

---
+++ disney_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  disney_ios
+//
+//  Created by Dale Evans on 29/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
